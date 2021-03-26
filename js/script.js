5abff50bd77f5c86cfd52a1b43de576c1daa03c3
Giovanni <giovanniberanrdi13@gmail.com>
2021-03-26 17:31:57 +0100

    exercise2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //EXERCISE1
-function es1() {
+function exercise1() {
 // Creare un array di oggetti: ogni oggetto descriverà 
 // una bike da corsa con le seguenti proprietà: name e weight. 
 // Stampare a schermo la bici con peso minore.
@@ -31,15 +31,55 @@ function es1() {
         }
     }
     console.log(lightestBike); //EXPECTED LOG:
-                            //    {name: "bike3", weight: 2.5}
-                            //    'name': 'bike3',
-                            //    'weight': 2.5
+                               // {name: "bike3", weight: 2.5}
+                               // 'name': 'bike3',
+                               // 'weight': 2.5
   }
 
-  function init() {
-    es1();
+//--------------------------------------------------------------//
+
+//ESERCIZIO2
+function exercise2() {
+    // Creare un array di oggetti di squadre di calcio. 
+    // Ogni squadra avrà diverse proprietà: 
+    // nome, punti fatti, falli subiti.
+    // Nome sarà l'unica proprietà da compilare, 
+    // le altre saranno tutte settate a 0.
+
+    let teams = [
+        {
+            'name': 'Lucchese',
+            'score': 0,
+            'fouls': 0
+        },
+        {
+            'name': 'Civitavecchia',
+            'score': 0,
+            'fouls': 0
+        },
+        {
+            'name': 'Pistoiese',
+            'score': 0,
+            'fouls': 0
+        },
+        {
+            'name': 'TeamOlesto',
+            'score': 0,
+            'fouls': 0
+        }
+    ];                      
+    console.log(teams);     //EXPECTED LOG
+                            //0: {name: "Lucchese", score: 0, fouls: 0}
+                            //1: {name: "Civitavecchia", score: 0, fouls: 0}
+                            //2: {name: "Pistoiese", score: 0, fouls: 0}
+                            //3: {name: "TeamOlesto", score: 0, fouls: 0}
 }
 
+//FUNCTIONS INIT
+function init() {
+    //exercise1();
+    exercise2()
+}
 
 // JQUERY
 $(document).ready(init);
